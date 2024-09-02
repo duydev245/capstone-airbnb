469073173cd030764fd47492b1985be9e1d9f6bd
duydev245 <danniduy.work@gmail.com>
2024-09-02 21:54:02 +0700

    fix deploy

--- src/modules/User/RoomsList/RoomsList.jsx
+++ src/modules/User/RoomsList/RoomsList.jsx
@@ -42,6 +42,26 @@ const RoomsList = () => {
 
     const filterRooms = roomsById?.filter((room) => {
 
+        // if (searchParams) {
+
+        //     if (searchParams.guest === room.khach) {
+        //         return true
+        //     } else if (!searchParams.guest) {
+        //         return true
+        //     }
+        // } else {
+        //     return true
+        // }
+
+        // if (filters.khach === room.khach || filterRooms.giuong === room.giuong || filters.phongNgu === room.phongNgu || filterRooms.phongTam === room.phongTam) {
+        //     return true
+        // }
+
+        // if (showAll) {
+        //     return true;
+
+        // }
+
         const matchKhach = !filters.khach || room.khach === filters.khach;
         const matchGiuong = !filters.giuong || room.giuong === filters.giuong;
         const matchPhongNgu = !filters.phongNgu || room.phongNgu === filters.phongNgu;
